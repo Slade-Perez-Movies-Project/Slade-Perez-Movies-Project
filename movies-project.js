a5cc648ec91db38d62655bc7bb865ffe00116215
Hector Perez <hector.elliott.perez@gmail.com>
2022-11-02 14:45:08 -0700

    fetch function returns entire list of movies and presents

--- movies-project.js
+++ movies-project.js
@@ -1,1 +1,21 @@
-console.log("test");
+let url = 'https://faithful-marsh-soprano.glitch.me/movies'
+
+// Retrieve Movies List
+// todo: sort to present alphabetically
+// todo: create function to create cards under data.map
+// todo: loading animation
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let div = document.createElement('div');
+        console.log(data);
+        let mov;
+        data.map(function(movieObj) {
+                mov = `Movie Title: ${movieObj.title}  -  ID: ${movieObj.id}  -  Rating: ${movieObj.rating}  -  Genre: ${movieObj.genre}`
+                div.append(mov);
+                document.body.append(div);
+        })
+        // mov = data[0].title + data[0].id;
+            console.log(mov);
+    });
+
