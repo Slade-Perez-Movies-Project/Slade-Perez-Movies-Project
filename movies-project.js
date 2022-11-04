79f6bde819aa739be2a08e8bd425c436db2a382f
Hector Perez <hector.elliott.perez@gmail.com>
2022-11-04 13:29:45 -0700

    refactored cards, got rid of strings

--- movies-project.js
+++ movies-project.js
@@ -1,12 +1,14 @@
 
 let url = 'https://faithful-marsh-soprano.glitch.me/movies'
 let movieList = document.getElementById('movieList')
+let moviesArray = [];
 
 // Page Loading Animation
 window.onload = function(){
     $('.loader-wrapper').fadeOut('slow');
     document.getElementById("my_audio").play();
 };
+
 //Delete a movie
 const deleteMovie = async (id) => {
     try{
@@ -20,60 +22,89 @@ const deleteMovie = async (id) => {
     }
 }
 
+const clearMovies = () => {
+    let div = document.getElementById("movieList");
+    div.innerHTMl = '';
+}
+
 // Retrieve Movies List
 // todo: sort to present alphabetically
 // todo: create function to create cards under data.map
+
 fetch(url)
     .then(response => response.json())
     .then(data => {
-        let div = document.createElement('div');
-        div.classList.add("container");
-        let mov;
+        clearMovies();
+
         data.map(function(movieObj) {
-            let container = document.createElement('div');
-            container.classList.add("d-inline-block", "justify-content-center");
-            container.innerHTML = '';
-            let cardHTML = `<div class="card m-3" style="width: 18rem">`;
-            cardHTML += `<img src="img/matrix-niccage.jpeg" class="card-img-top" alt="...">`;
-            cardHTML += `<div class="card-body">`;
-            cardHTML += `<h5 class="card-title">${movieObj.title}  -  ID: ${movieObj.id}</h5>`;
-            cardHTML += `<input type="hidden" id="movieId${movieObj.id}" value="${movieObj.id}">`;
-            cardHTML += `<p class="card-text">Rating: ${movieObj.rating} Genre: ${movieObj.genre}</p>`;
-            cardHTML += `<a href="#" class="btn btn-primary">Edit</a>`;
-            cardHTML += `<a href="#" class="btn btn-primary delete-btn" value="${movieObj.id}">Delete</a></div></div>`;
-            // let movieCard = document.createElement('div');
-            // movieCard.classList.add("card");
-            // movieCard = `Movie Title: ${movieObj.title}  -  ID: ${movieObj.id}  -  Rating: ${movieObj.rating}  -  Genre: ${movieObj.genre}`
-            // movieList.innerHTML(cardHTML);
-            container.innerHTML = cardHTML;
-            document.body.append(container);
-            //$('.delete-btn').click(function(e){
-            //    e.preventDefault();
-            //    const nicRageSound = new Audio('audio/that-was-wrong-what-you-did.mp3');
-            //    nicRageSound.play();
-            //    const delOptions = {
-            //        method: 'DELETE',
-            //        headers: {
-            //            'Content-Type': 'application/json',
-            //        },
-            //    }
-            //    fetch(`https://faithful-marsh-soprano.glitch.me/movies/${movieObj}`, delOptions)
-            //        .then(response => response.json())
-            //        .catch(response => console.log(response))
-            //});
-            //DELETE things
-            $('.delete-btn').onclick = async e => {
+            let container = document.getElementById('movieList');
+
+            // Creating Card
+            let cardHTML = document.createElement("div");
+            cardHTML.classList.add("card", "m-3");
+            cardHTML.style = ("width: 18rem");
+
+            // Create Card Body
+            let cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
+
+            // Create Card IMG
+            let cardImg = document.createElement("img");
+            cardImg.classList.add("card-img-top");
+            cardImg.src = ("img/matrix-niccage.jpeg");
+            cardImg.alt = ("...");
+
+
+            // Create Card Title
+            let cardTitle = document.createElement("h5");
+            cardTitle.classList.add("card-title");
+            cardTitle.innerText = movieObj.title
+
+            // Create Card Description
+            let cardDescription = document.createElement("p");
+            cardDescription.classList.add("card-text");
+            cardDescription.innerText = movieObj.rating;
+
+            // Create Edit Button
+            let editBtn = document.createElement("a");
+            editBtn.href = ("#");
+            editBtn.classList.add("btn");
+            editBtn.innerText = ("Edit");
+
+            // Create Delete Button
+            let delBtn = document.createElement("a");
+            delBtn.classList.add("btn");
+            delBtn.innerText= ("Delete");
+
+
+            $(delBtn).click( async e => {
                 console.log("test")
                 e.preventDefault();
                 const nicRageSound = new Audio('audio/that-was-wrong-what-you-did.mp3');
                 nicRageSound.play();
-                let confirm = window.confirm(`Are you sure you want to delete ${movie.title}?`)
+                let confirm = window.confirm(`Are you sure you want to delete ${movieObj.title}?`)
                 if (confirm) {
-                    await deleteMovie(movie.id)
+                    await deleteMovie(movieObj.id)
                 }
-            }
+            })
+
+
+            // Append Everything
+            document.body.append(container);
+            container.appendChild(cardBody);
+
+
+            cardBody.append(cardImg);
+            cardBody.append(cardTitle);
+            cardBody.append(cardDescription);
+            cardBody.append(editBtn);
+            cardBody.append(delBtn);
+
         });
+
     });
+
+// fetchMovies();
 
 
 // Post New Movie
