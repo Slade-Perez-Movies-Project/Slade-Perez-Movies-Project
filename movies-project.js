43f3dedb63eaef0d836f7235ceb5b5a695b257f6
Hector Perez <hector.elliott.perez@gmail.com>
2022-11-04 14:22:22 -0700

    cards presenting

--- movies-project.js
+++ movies-project.js
@@ -31,80 +31,82 @@ const clearMovies = () => {
 // todo: sort to present alphabetically
 // todo: create function to create cards under data.map
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        clearMovies();
+const presentMovies = () => {
+    // Grab Placeholder Div from HTML
+    let container = document.getElementById('movieList');
+    container.classList.add("flex-wrap", "align-items-start");
 
-        data.map(function(movieObj) {
-            let container = document.getElementById('movieList');
+    clearMovies();
 
-            // Creating Card
-            let cardHTML = document.createElement("div");
-            cardHTML.classList.add("card", "m-3");
-            cardHTML.style = ("width: 18rem");
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            data.forEach(function(movieObj) {
 
-            // Create Card Body
-            let cardBody = document.createElement("div");
-            cardBody.classList.add("card-body");
+                // Create Card Body
+                let cardBody = document.createElement("div");
+                cardBody.classList.add("card-body");
 
-            // Create Card IMG
-            let cardImg = document.createElement("img");
-            cardImg.classList.add("card-img-top");
-            cardImg.src = ("img/matrix-niccage.jpeg");
-            cardImg.alt = ("...");
+                // Creating Card
+                let card = document.createElement("div");
+                card.classList.add("card", "m-3");
+                // card.style = ("width: 18rem;");
 
-
-            // Create Card Title
-            let cardTitle = document.createElement("h5");
-            cardTitle.classList.add("card-title");
-            cardTitle.innerText = movieObj.title
-
-            // Create Card Description
-            let cardDescription = document.createElement("p");
-            cardDescription.classList.add("card-text");
-            cardDescription.innerText = movieObj.rating;
-
-            // Create Edit Button
-            let editBtn = document.createElement("a");
-            editBtn.href = ("#");
-            editBtn.classList.add("btn");
-            editBtn.innerText = ("Edit");
-
-            // Create Delete Button
-            let delBtn = document.createElement("a");
-            delBtn.classList.add("btn");
-            delBtn.innerText= ("Delete");
+                // Create Card IMG
+                let cardImg = document.createElement("img");
+                cardImg.classList.add("card-img-top", "poster");
+                cardImg.src = ("img/matrix-niccage.jpeg");
+                cardImg.alt = ("...");
 
 
-            $(delBtn).click( async e => {
-                console.log("test")
-                e.preventDefault();
-                const nicRageSound = new Audio('audio/that-was-wrong-what-you-did.mp3');
-                nicRageSound.play();
-                let confirm = window.confirm(`Are you sure you want to delete ${movieObj.title}?`)
-                if (confirm) {
-                    await deleteMovie(movieObj.id)
-                }
-            })
+                // Create Card Title
+                let cardTitle = document.createElement("h5");
+                cardTitle.classList.add("card-title", "title");
+                cardTitle.innerText = movieObj.title
+
+                // Create Card Description
+                let cardDescription = document.createElement("p");
+                cardDescription.classList.add("card-text");
+                cardDescription.innerText = movieObj.rating;
+
+                // Create Edit Button
+                let editBtn = document.createElement("a");
+                editBtn.href = ("#");
+                editBtn.classList.add("btn", "btn-primary");
+                editBtn.innerText = ("Edit");
+
+                // Create Delete Button
+                let delBtn = document.createElement("a");
+                delBtn.classList.add("btn", "btn-success");
+                delBtn.innerText= ("Delete");
 
 
-            // Append Everything
-            document.body.append(container);
-            container.appendChild(cardBody);
+                $(delBtn).click( async e => {
+                    console.log("test")
+                    e.preventDefault();
+                    const nicRageSound = new Audio('audio/that-was-wrong-what-you-did.mp3');
+                    nicRageSound.play();
+                    let confirm = window.confirm(`Are you sure you want to delete ${movieObj.title}?`)
+                    if (confirm) {
+                        await deleteMovie(movieObj.id)
+                    }
+                })
 
 
-            cardBody.append(cardImg);
-            cardBody.append(cardTitle);
-            cardBody.append(cardDescription);
-            cardBody.append(editBtn);
-            cardBody.append(delBtn);
+                // Append Everything
 
+                cardBody.appendChild(cardImg);
+                cardBody.appendChild(cardTitle);
+                cardBody.appendChild(cardDescription);
+                cardBody.appendChild(editBtn);
+                cardBody.appendChild(delBtn);
+                card.appendChild(cardBody);
+                container.appendChild(card);
+            });
         });
-
-    });
-
-// fetchMovies();
+}
+presentMovies();
 
 
 // Post New Movie
@@ -138,41 +140,7 @@ $("#submit-btn").click(function(e) {
     // Present Movies to Include New Movie
     // todo: happening before post is complete, need find way to wait
     // todo: make a function
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // <div className="card" style="width: 18rem;">
-            //     <img src="..." className="card-img-top" alt="...">
-            //         <div className="card-body">
-            //             <h5 className="card-title">Card title</h5>
-            //             <p className="card-text">Some quick example text to build on the card title and make up the bulk
-            //                 of the card's content.</p>
-            //             <a href="#" className="btn btn-primary">Go somewhere</a>
-            //         </div>
-            // </div>
-            let container = document.createElement('div');
-            container.innerHTML = '';
 
-
-            data.map(function(movieObj) {
-                let container = document.createElement('div');
-                container.classList.add("d-inline-block", "justify-content-center");
-                container.innerHTML = '';
-                let cardHTML = `<div class="card m-3" style="width: 18rem">`;
-                cardHTML += `<img src="img/matrix-niccage.jpeg" class="card-img-top" alt="...">`;
-                cardHTML += `<div class="card-body">`;
-                cardHTML += `<h5 class="card-title">${movieObj.title}  -  ID: ${movieObj.id}</h5>`;
-                cardHTML += `<p class="card-text">Rating: ${movieObj.rating} Genre: ${movieObj.genre}</p>`;
-                cardHTML += `<a href="#" class="btn btn-primary">Edit</a>`;
-                cardHTML += `<a href="#" class="btn btn-primary">Delete</a></div></div>`;
-                // let movieCard = document.createElement('div');
-                // movieCard.classList.add("card");
-                // movieCard = `Movie Title: ${movieObj.title}  -  ID: ${movieObj.id}  -  Rating: ${movieObj.rating}  -  Genre: ${movieObj.genre}`
-                // movieList.innerHTML(cardHTML);
-                container.innerHTML = cardHTML;
-                document.body.append(container);
-            })
-        });
 })
 
 
