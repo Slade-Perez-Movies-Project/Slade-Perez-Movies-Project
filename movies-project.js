6b197a6af740fe79321d0d69d1c89b85eb839ce7
Hector Perez <hector.elliott.perez@gmail.com>
2022-11-06 04:45:54 -0800

    edit does not work, clear div function doesn't work

--- movies-project.js
+++ movies-project.js
@@ -1,23 +1,85 @@
 
-let url = 'https://faithful-marsh-soprano.glitch.me/movies'
-let movieList = document.getElementById('movieList')
+let url = 'https://faithful-marsh-soprano.glitch.me/movies/'
 var editMovieModal = document.getElementById("editMovieModal");
+var search = document.querySelector('#movie-search');
+var genreFilter = document.querySelector("#filter-genre");
+var ratingFilter = document.querySelector("#filter-rating");
+const loader = document.querySelector("#loading")
 
-// Page Loading Animation
-window.onload = function(){
-    $('.loader-wrapper').fadeOut('slow');
-    // document.getElementById("my_audio").play();
-};
+
+// Loading Animation Functions
+function displayLoading() {
+    loader.classList.add("display");
+    setTimeout(() => {
+        loader.classList.remove("display");
+    }, 5000);
+}
+function hideLoading() {
+    loader.classList.remove("display");
+}
+
+
+// Movie Search Function
+$(".search-btn").click(function(e) {
+    e.preventDefault();
+    let searchedStr = search.value;
+})
+
+// Filter By Genres
+$("#submit-genre-filter").click(function(e) {
+    e.preventDefault();
+    let genreChoice = genreFilter.value;
+})
+
+// Filter By Ratings
+$("#submit-rating-filter").click(function(e) {
+    e.preventDefault();
+    let ratingChoice = ratingFilter.value;
+})
+
+// Add New Movie
+$("#new-submit-btn").click(function(e) {
+    e.preventDefault();
+    const searchSound = new Audio("audio/search.wav");
+    searchSound.play();
+
+    // Set Movie Object and Properties
+    let moviePost = {
+        title: $("#new-movie-title").val(),
+        genre: $("#new-movie-genre").val(),
+        rating: $("#new-rating").val(),
+    };
+
+    // Set fetch Options for POST method
+    const postOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(moviePost),
+    };
+
+    // Post New Movie
+    fetch(url, postOptions)
+        .then(response => response.json())
+        .catch(response => console.log(response))
+
+    presentMovies();
+
+    // Present Movies to Include New Movie
+    // todo: happening before post is complete, need find way to wait
+})
+
 
 // Delete movie
 const deleteMovie = async (id) => {
-    try{
-        let deleteRequest = await fetch(url + `/${id}`,{
+    try {
+        let deleteRequest = await fetch(url + `${id}`,{
             method : 'DELETE'
         })
 
         return deleteRequest
-    }catch(err){
+    } catch(err) {
         console.error(err)
     }
 }
@@ -26,12 +88,12 @@ const deleteMovie = async (id) => {
 // Edit Movie
 const editMovie = async(id, movieObj) => {
     try{
-        let editRequest = await fetch(url + `/${id}`, {
-            method: 'POST',
-            body: JSON.stringify(movieObj),
+        let editRequest = await fetch(url + `${id}`, {
+            method: 'PUT',
             headers: {
                 "Content-Type": "application/json"
-            }
+            },
+            body: JSON.stringify(movieObj),
         })
         return editRequest
     } catch(err) {
@@ -39,6 +101,8 @@ const editMovie = async(id, movieObj) => {
     }
 }
 
+
+// Clear Movie List Div -- Not Working
 const clearMovies = () => {
     let div = document.getElementById("movieList");
     div.innerHTMl = '';
@@ -47,18 +111,19 @@ const clearMovies = () => {
 // Retrieve Movies List
 // todo: sort to present alphabetically
 // todo: create function to create cards under data.map
-
 const presentMovies = () => {
     // Grab Placeholder Div from HTML
     let container = document.getElementById('movieList');
     container.classList.add("flex-wrap", "align-items-start");
 
+    displayLoading();
     clearMovies();
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            data.forEach(function(movieObj) {
+            hideLoading();
+            data.map(function(movieObj) {
 
                 // Create Card Body
                 let cardBody = document.createElement("div");
@@ -66,7 +131,7 @@ const presentMovies = () => {
 
                 // Creating Card
                 let card = document.createElement("div");
-                card.classList.add("card", "m-3", "d-flex");
+                card.classList.add("card", "movie-card", "m-3", "d-flex", "bg-light");
                 // card.style = ("width: 18rem;");
 
                 // Create Card IMG
@@ -74,7 +139,6 @@ const presentMovies = () => {
                 cardImg.classList.add("card-img-top", "poster");
                 cardImg.src = ("img/matrix-niccage.jpeg");
                 cardImg.alt = ("...");
-
 
                 // Create Card Title
                 let cardTitle = document.createElement("h5");
@@ -90,12 +154,16 @@ const presentMovies = () => {
                 let editBtn = document.createElement("a");
                 editBtn.href = ("#");
                 editBtn.classList.add("btn", "btn-primary", "d-inline-flex", "justify-content-start");
-                editBtn.innerText = ("Edit");
+                editBtn.innerHTML = ("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pencil-fill\" viewBox=\"0 0 16 16\">\n" +
+                    "  <path d=\"M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z\"/>\n" +
+                    "</svg>");
 
                 // Create Delete Button
                 let delBtn = document.createElement("a");
                 delBtn.classList.add("btn", "btn-success", "d-inline-flex", "justify-content-end");
-                delBtn.innerText= ("Delete");
+                delBtn.innerHTML= ("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-trash3-fill\" viewBox=\"0 0 16 16\">\n" +
+                    "  <path d=\"M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z\"/>\n" +
+                    "</svg>");
 
 
                 $(delBtn).click( async e => {
@@ -104,7 +172,8 @@ const presentMovies = () => {
                     nicRageSound.play();
                     let confirm = window.confirm(`Are you sure you want to delete ${movieObj.title}?`)
                     if (confirm) {
-                        await deleteMovie(movieObj.id)
+                        await deleteMovie(movieObj.id);
+                        await presentMovies();
                     }
                 })
 
@@ -122,7 +191,8 @@ const presentMovies = () => {
                         document.getElementById("edit-genre").value = `${movieObj.genre}`
                         document.getElementById("edit-rating").value = `${movieObj.rating}`;
 
-                        await editMovie(`${movieObj.id}`, movieObj)
+                        await editMovie(movieObj.id, movieObj);
+                        await presentMovies();
                     }
                 })
 
@@ -141,38 +211,49 @@ const presentMovies = () => {
 presentMovies();
 
 
-// Post New Movie
-$("#new-submit-btn").click(function(e) {
+// Edit Movie
+$("#edit-submit-btn").click(function(e) {
     e.preventDefault();
     const searchSound = new Audio("audio/search.wav");
     searchSound.play();
 
     // Set Movie Object and Properties
-    let moviePost = {};
-    let movieTitle = document.getElementById("new-movie-title").value;
-    let genre = document.getElementById("new-movie-genre").value;
-    let rating = document.getElementById("new-rating").value;
-    moviePost.title = movieTitle;
-    moviePost.genre = genre;
-    moviePost.rating = rating;
+    let moviePut = {};
+    let movieTitle = document.getElementById("edit-title").value;
+    let genre = document.getElementById("edit-genre").value;
+    let rating = document.getElementById("edit-rating").value;
+    moviePut.title = movieTitle;
+    moviePut.genre = genre;
+    moviePut.rating = rating;
+    console.log(moviePut);
 
     // Set fetch Options for POST method
-    const postOptions = {
-        method: 'POST',
+    const putOptions = {
+        method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(moviePost),
+        body: JSON.stringify(moviePut),
     };
 
-    // Post New Movie
-    fetch(url, postOptions)
+    // PUT New Movie
+    fetch(url , putOptions)
         .then(response => response.json())
         .catch(response => console.log(response))
 
     // Present Movies to Include New Movie
     // todo: happening before post is complete, need find way to wait
 })
+
+// Page Loading Animation
+window.onload = function(){
+    loader.classList.add("display");
+    setTimeout(() => {
+        loader.classList.remove("display");
+    }, 5000);
+    // document.getElementById("my_audio").play();
+};
+
 
 // Modal Functionality
 // Get the modal
@@ -185,7 +266,7 @@ var newSubmitBtn = document.getElementById("new-submit-btn");
 var editSubmitBtn = document.getElementById("edit-submit-btn");
 
 // Get the <div> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var closeNewModal = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 newMovieBtn.onclick = function() {
@@ -203,7 +284,7 @@ editSubmitBtn.onclick = function(e) {
 
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeNewModal.onclick = function() {
     newMovieModal.style.display = "none";
 }
 
