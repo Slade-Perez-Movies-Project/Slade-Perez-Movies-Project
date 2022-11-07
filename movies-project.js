8226ff62484a58bd3564a570961301d1ddbf4c9e
Hector Perez <hector.elliott.perez@gmail.com>
2022-11-07 05:47:34 -0800

    cleaned up code

--- movies-project.js
+++ movies-project.js
@@ -1,92 +1,69 @@
-
+// ASSIGNING VARIABLES //
 let url = 'https://faithful-marsh-soprano.glitch.me/movies/'
+// Modals
+var newMovieBtn = document.getElementById("newMovieBtn");
+var newMovieModal = document.getElementById("newMovieModal");
+var newSubmitBtn = document.getElementById("new-submit-btn");
+var closeNewModal = document.getElementsByClassName("close")[0];
 var editMovieModal = document.getElementById("editMovieModal");
+var editSubmitBtn = document.getElementById("edit-submit-btn");
+// Search & Filter
 var search = document.querySelector('#movie-search');
 var genreFilter = document.querySelector("#filter-genre");
 var ratingFilter = document.querySelector("#filter-rating");
-const loader = document.querySelector("#loading");
+// Movies
 var currentMovie = null;
 var movieCollection = [];
+// Loading
+const loader = document.querySelector("#loading");
 
+// ---- //
 
-// Loading Animation Functions
-function displayLoading() {
-    loader.classList.add("display");
-    setTimeout(() => {
-        loader.classList.remove("display");
-    }, 5000);
-}
-function hideLoading() {
-    loader.classList.remove("display");
-}
-
-function renderSearchedMovies(arr) {
-    let html = '';
-
-    html += `<div class="card-body p-0 border-0">`
-    html += `<div class="card movie-card m-3 d-flex bg-light text-center border-0 grow">`
-    html += `<img class="card-img-top poster" src="img/matrix-niccage.jpeg">`
-    html += `<h5 class="card-title title m-2 grow">${arr.title}</h5>`
-    html += `<p class="card-text grow">${arr.rating} Stars -- ${arr.genre}</p>`
-    html += `<a class="btn btn-dark alter-movie d-inline-flex p-1 rounded-circle bottom" href="#">Trash Btn</a>`
-    html += `<a class="btn btn-dark alter-movie d-inline-flex p-1 rounded-circle bottom">Edit Btn</a>`
-    html += `</div>`
-    html += `</div>`
-
-    return html
-}
-
-function searchResults(arr) {
-    let html = '';
-    for (let i = 0; i < arr.length; i++) {
-        html += renderSearchedMovies(arr[i]);
-    }
-    return html;
-}
-
+// -- SEARCH & FILTER FUNCTIONALITY -- //
+// SEARCH BAR FUNCTION //
+// 1A. Collected Searched String
+$("#movie-search").keyup(function(e) {
+    e.preventDefault();
+    searchMovies(search.value);
+})
+// 2A. Create Array with Matching Searched Characters
 function searchMovies(searchedStr) {
     let matchArray = [];
-
     movieCollection.forEach(function (movieObj) {
         if (movieObj.title.toLowerCase().includes(searchedStr.toLowerCase())) {
             matchArray.push(movieObj);
         }
     })
-    // $("#movieList").html(searchResults(matchArray));
     $("#movieList").html((searchResults(matchArray)))
 }
 
-// Movie Search Function
-$("#movie-search").keyup(function(e) {
+// FILTER BY GENRE //
+// 1B. Filter By Genres
+$("#filter-genre").change(function(e) {
     e.preventDefault();
-    searchMovies(search.value);
+    searchGenre(genreFilter.value)
 })
-
-
+// 2B. Create Array of Movies with Matching Genres
 function searchGenre(movieGenre) {
     let genreArray = [];
-    movieCollection.forEach(function (movieObj) {
+    movieCollection.map(function (movieObj) {
         if (movieObj.genre.toLowerCase().includes(movieGenre.toLowerCase())) {
             genreArray.push(movieObj);
-            console.log(genreArray);
         }
     })
-    // $("#movieList").html(searchResults(matchArray));
     $("#movieList").html((searchResults(genreArray)))
 }
 
-
-// Filter By Genres
-$("#filter-genre").change(function(e) {
+// FILTER BY RATING //
+// 1C. Filter By Ratings
+$("#filter-rating").change(function(e) {
     e.preventDefault();
-    let genreChoice = genreFilter.value;
-    console.log(genreChoice);
-    searchGenre(genreFilter.value)
+    searchRatings(ratingFilter.value)
 })
-
+// 2C. Create Array of Movies with Matching Ratings
 function searchRatings(range) {
     let genreArray = [];
-    movieCollection.forEach(function (movieObj) {
+    movieCollection.map(function (movieObj) {
         if (movieObj.rating === range) {
             genreArray.push(movieObj);
         }
@@ -94,12 +71,39 @@ function searchRatings(range) {
     $("#movieList").html((searchResults(genreArray)))
 }
 
-// Filter By Ratings
-$("#filter-rating").change(function(e) {
-    e.preventDefault();
-    searchRatings(ratingFilter.value)
-})
+// PRESENT RESULTS //
+// 3. Iterate Through Array
+function searchResults(arr) {
+    let html = '';
+    for (let i = 0; i < arr.length; i++) {
+        html += renderSearchedMovies(arr[i]);
+    }
+    return html;
+}
+// 4. Render
+function renderSearchedMovies(arr) {
+    let html = '';
+    html += `<div class="card-body d-flex justify-end p-0 border-0">`
+    html += `<div class="card movie-card m-3 d-flex bg-light text-center border-0 grow">`
+    html += `<img class="card-img-top poster" src="img/matrix-niccage.jpeg">`
+    html += `<h5 class="card-title title m-2">${arr.title}</h5>`
+    html += `<p class="card-text">${arr.rating} Stars -- ${arr.genre}</p>`
+    html += `<a class="btn btn-dark alter-movie d-inline-flex m-0 p-1 rounded-circle justify-content-center" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+      <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+       </svg>
+        </a>`
+    html += `<a class="btn btn-dark alter-movie d-inline-flex m-0 p-1 rounded-circle d-flex justify-content-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+      <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+        </svg></a>`
+    html += `</div>`
+    html += `</div>`
 
+    return html
+}
+
+// ---- //
+
+// MOVIE MANIPULATION //
 // Grab Updated Movies
 const updateMovies = async () => {
     try {
@@ -109,7 +113,10 @@ const updateMovies = async () => {
     }catch(err){
         console.error(err)
     } }
-
+// Clear Movie List
+const clearMovies = () => {
+    $("#movieList").empty();
+}
 // Add New Movie
 $("#new-submit-btn").click(async function(e) {
     e.preventDefault();
@@ -144,8 +151,6 @@ $("#new-submit-btn").click(async function(e) {
     // Present Movies to Include New Movie
     // todo: happening before post is complete, need find way to wait
 })
-
-
 // Delete movie
 const deleteMovie = async (id) => {
     try {
@@ -156,8 +161,6 @@ const deleteMovie = async (id) => {
         console.error(err)
     }
 }
-
-
 // Edit Movie
 const editMovie = async(id, movieObj) => {
     try{
@@ -173,11 +176,53 @@ const editMovie = async(id, movieObj) => {
     }
 }
 
+// ---- //
 
-// Clear Movie List Div
-const clearMovies = () => {
-    $("#movieList").empty();
+// MODAL FUNCTIONALITY //
+// Open New Modal on Click
+newMovieBtn.onclick = function() {
+    newMovieModal.style.display = "block";
 }
+// Close New Modal After Submit
+newSubmitBtn.onclick = function() {
+    newMovieModal.style.display = "none";
+}
+// Close New Modal on 'x' Click
+closeNewModal.onclick = function() {
+    newMovieModal.style.display = "none";
+}
+// Close New Modal on Outside Click
+window.onclick = function(event) {
+    if (event.target == newMovieModal) {
+        newMovieModal.style.display = "none";
+    }
+}
+// Close Edit Modal On Submit
+editSubmitBtn.onclick = function(e) {
+    e.preventDefault()
+    editMovieModal.style.display = "none";
+}
+
+// ---- //
+
+// Loading Animation Functions
+function displayLoading() {
+    loader.classList.add("display");
+    setTimeout(() => {
+        loader.classList.remove("display");
+    }, 5000);
+}
+function hideLoading() {
+    loader.classList.remove("display");
+}
+window.onload = function(){
+    loader.classList.add("display");
+    setTimeout(() => {
+        loader.classList.remove("display");
+    }, 5000);
+};
+
+// ---- //
 
 // Retrieve Movies List
 const presentMovies = () => {
@@ -222,14 +267,14 @@ const presentMovies = () => {
                 // Create Edit Button
                 let editBtn = document.createElement("a");
                 editBtn.href = ("#");
-                editBtn.classList.add("btn", "btn-dark", "alter-movie", "d-inline-flex","p-1", "rounded-circle", "bottom");
+                editBtn.classList.add("btn", "btn-dark", "alter-movie", "d-inline-flex","p-1", "rounded-circle");
                 editBtn.innerHTML = ("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pencil-fill\" viewBox=\"0 0 16 16\">\n" +
                     "  <path d=\"M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z\"/>\n" +
                     "</svg>");
 
                 // Create Delete Button
                 let delBtn = document.createElement("a");
-                delBtn.classList.add("btn", "btn-dark", "alter-movie", "d-inline-flex", "p-1", "rounded-circle", "bottom");
+                delBtn.classList.add("btn", "btn-dark", "alter-movie", "d-inline-flex", "p-1", "rounded-circle");
                 delBtn.innerHTML= ("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-trash3-fill\" viewBox=\"0 0 16 16\">\n" +
                     "  <path d=\"M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z\"/>\n" +
                     "</svg>");
@@ -303,63 +348,3 @@ const presentMovies = () => {
 }
 
 presentMovies();
-
-
-// Page Loading Animation
-window.onload = function(){
-    loader.classList.add("display");
-    setTimeout(() => {
-        loader.classList.remove("display");
-    }, 5000);
-    // document.getElementById("my_audio").play();
-};
-
-
-// Modal Functionality
-// Get the modal
-var newMovieModal = document.getElementById("newMovieModal");
-
-// Get the button that opens the modal
-var newMovieBtn = document.getElementById("newMovieBtn");
-
-var newSubmitBtn = document.getElementById("new-submit-btn");
-var editSubmitBtn = document.getElementById("edit-submit-btn");
-
-// Get the <div> element that closes the modal
-var closeNewModal = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-newMovieBtn.onclick = function() {
-    newMovieModal.style.display = "block";
-}
-
-newSubmitBtn.onclick = function() {
-    newMovieModal.style.display = "none";
-}
-
-editSubmitBtn.onclick = function(e) {
-    e.preventDefault()
-    editMovieModal.style.display = "none";
-}
-
-
-// When the user clicks on <span> (x), close the modal
-closeNewModal.onclick = function() {
-    newMovieModal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == newMovieModal) {
-        newMovieModal.style.display = "none";
-    }
-}
-
-
-// IDEAS:
-// Sound bites
-// Highlight reels to preview
-// Add Movie Posters
-// Customize Ratings Bar
-// Optional Image URL for New Movie
-
